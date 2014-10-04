902f0567b9900df1d2839f3ed769fb04c17d6b1e
Denise Che <cdenise@mit.edu>
2014-10-04 18:27:29 -0400

    posting positions

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os');
 
 router.get('/', function(req, res) {
 	res.render('admin/index', {});
@@ -7,20 +8,30 @@ router.get('/', function(req, res) {
 
 
 router.post('/positions', function(req, res, next) {
-	// var db = req.db;
- //  	var students = db.get('people');
-	// students.insert({"name": req.body.user, "age": req.body.user_age}, function(err, docs){
-	// 	if(err){
-	// 		res.send("There was a problem");
-	// 	}else{
-	// 		res.redirect("/users");
-	// 	}
-	// });
-});
-
+	var db  = req.db;
+	var ballot = db.get('ballot');
+	ballot.remove();
+	var input = req.body.positions;
+	var lines = input.split(os.EOL);
+	for (var i=0; i<lines.length; i++){
+		line = lines[i].split(":");
+		var position = line[0];
+		var candidateList = line[1].split(";");
+		for (var j=0; j<candidateList.length; j++){
+			var candidate = candidateList[j];
+			ballot.insert({"position": position, "candidate": candidate, "vote1": 0, "vote2":0}, function(err, entry){});			
+		}
+	}
+	ballot.find({}, function(err, wholeBallot){
+		console.log(wholeBallot);
+	});	
+})
 router.post('/kerberos', function(req, res, next) {
-	// var db = req.db;
- //  	var students = db.get('people');
+
+
+
+
+
 	// students.insert({"name": req.body.user, "age": req.body.user_age}, function(err, docs){
 	// 	if(err){
 	// 		res.send("There was a problem");
