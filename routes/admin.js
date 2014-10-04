841b3cfad87b8768b2e723b9d59d301ea194e2da
Denise Che <cdenise@mit.edu>
2014-10-04 16:24:25 -0400

    admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,27 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  var db = req.db;
-  var students = db.get('people');
-  students.find({}, function(e, docs){
-  	res.render('users/index', { title: 'Users', 'individuals': docs });
-  });
+	res.render('admin/index', {});
 });
 
-router.get('/new', function(req, res) {
-	res.render('users/new', { title: 'Add New User' });
+
+router.post('/positions', function(req, res, next) {
+	// var db = req.db;
+ //  	var students = db.get('people');
+	// students.insert({"name": req.body.user, "age": req.body.user_age}, function(err, docs){
+	// 	if(err){
+	// 		res.send("There was a problem");
+	// 	}else{
+	// 		res.redirect("/users");
+	// 	}
+	// });
 });
 
-router.post('/create', function(req, res, next) {
-	var db = req.db;
-  	var students = db.get('people');
-	students.insert({"name": req.body.user, "age": req.body.user_age}, function(err, docs){
-		if(err){
-			res.send("There was a problem");
-		}else{
-			res.redirect("/users");
-		}
-	});
+router.post('/kerberos', function(req, res, next) {
+	// var db = req.db;
+ //  	var students = db.get('people');
+	// students.insert({"name": req.body.user, "age": req.body.user_age}, function(err, docs){
+	// 	if(err){
+	// 		res.send("There was a problem");
+	// 	}else{
+	// 		res.redirect("/users");
+	// 	}
+	// });
 });
 
 module.exports = router;
