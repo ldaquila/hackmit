ccddad9dafd8ec104bf3f300a6a909229299428b
Laura D'Aquila <ldaquila@mit.edu>
2014-10-25 19:24:35 -0400

    admin auth

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var mongo = require('mongodb');
 var monk = require('monk');
@@ -37,6 +38,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'keyboard cat'}));
 
 app.use(function(req,res,next){
     req.db = db;
--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var os = require('os');
 
-router.get('/', function(req, res) {
+router.get('/', checkAuth, function(req, res) {
 	var voters = req.db.get('voters');
 	voters.find({}, function(err, docs){
 		console.log(docs);
@@ -10,7 +10,7 @@ router.get('/', function(req, res) {
 	res.render('admin/index', {});
 });
 
-router.get('/results', function(req, res) {
+router.get('/results', checkAuth, function(req, res) {
 	var ballot = req.db.get('ballot');
 	ballot.find({}, function(err, docs){
 		console.log(docs);
@@ -19,7 +19,7 @@ router.get('/results', function(req, res) {
 });
 
 
-router.post('/positions', function(req, res, next) {
+router.post('/positions', checkAuth, function(req, res, next) {
 	var db  = req.db;
 	var ballot = db.get('ballot');
 	var positionNames = db.get('positionNames');
@@ -44,7 +44,7 @@ router.post('/positions', function(req, res, next) {
 		console.log(doc);
 	});	
 })
-router.post('/kerberos', function(req, res, next) {
+router.post('/kerberos', checkAuth, function(req, res, next) {
 	var voters = req.db.get('voters');
 	var kerberosList = req.body.kerberos.split(os.EOL);
 
@@ -63,6 +63,29 @@ router.post('/kerberos', function(req, res, next) {
   	res.redirect("/admin");
 
 });
+
+router.get('/login', function(req, res) {
+	res.render('admin/login', {});
+});
+
+/* POST login page. */
+router.post('/login', function(req, res) {
+  if (req.body.password === "ttt") {
+  	req.session.user_id = "ttt";
+    res.redirect('/admin');
+  } else {
+  	res.render('admin/login', {  });
+  }
+});
+
+// checkAuth ensures that a user is logged in. If no user is logged in, redirects to the login page.
+function checkAuth(req, res, next) {
+  if (!req.session.user_id) {
+    res.redirect("/admin/login");
+  } else {
+    next();
+  }
+}
 
 /*function makeid()
 {
